52dc03ff0e0c1e3293f115e973d4d60732bf2f51
Ayush-Manware <ayushmanware19@gmail.com>
2024-02-07 18:39:43 +0530

    About us Page

--- script.js
+++ script.js
@@ -30,3 +30,23 @@ function hide() {
 }
 
 
+// ------------------------------ABOUT US SLIDER-------------------
+const slides = document.querySelectorAll('.slide');
+let currentSlide = 0;
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        slide.style.display = i === index ? 'block' : 'none';
+    });
+}
+
+function nextSlide() {
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide(currentSlide);
+}
+
+// Change slide every 3 seconds (adjust as needed)
+setInterval(nextSlide, 3000);
+
+
+
